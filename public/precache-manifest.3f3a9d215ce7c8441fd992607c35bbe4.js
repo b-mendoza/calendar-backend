d0151ac7fbbb04fabf6972505690227670f510ef
b-mendoza <bmendoza.dev@hotmail.com>
2020-10-03 11:06:23 -0600

    react front-end added

---
+++ public/precache-manifest.3f3a9d215ce7c8441fd992607c35bbe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb19b84878aff1b2c7659e3bf7bae52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0048f1838be3924f31bb",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "60ddb1490948b06aa59a",
+    "url": "/static/css/main.406cabd4.chunk.css"
+  },
+  {
+    "revision": "0048f1838be3924f31bb",
+    "url": "/static/js/2.bfebd712.chunk.js"
+  },
+  {
+    "revision": "104eb9125887548de959dc29f262ae56",
+    "url": "/static/js/2.bfebd712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ddb1490948b06aa59a",
+    "url": "/static/js/main.a5aa1afa.chunk.js"
+  },
+  {
+    "revision": "9fd970e1f25c0cd7eb0a",
+    "url": "/static/js/runtime-main.470b33ba.js"
+  }
+]);
